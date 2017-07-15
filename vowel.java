01eca30b6a220531f8847d8f190498fc6d8709c1
andriilguvi <andriilguvi68@gmail.com>
2017-07-15 09:54:45 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class vowel
+{
+public static void main(String args[])
+{
+String str="andriil";
+int vowel=0
+char[] c=str.toCharArray();
+for(char a:c)
+{
+if(Pattern matches("[aeiou]",Character.toString(a));
+System.out.print("vowel");
+else
+System.out.print("not a vowel");
+}
+}
+}
